b335e641479884e5a8f1a9f09df75b0368120161
PHIDELIST <phidelisoluoch@gmail.com>
2024-12-07 14:00:56 +0300

    collect active request metrics

--- prometheus-grafana/dotnet-api/Services/PrometheusService.cs
+++ prometheus-grafana/dotnet-api/Services/PrometheusService.cs
@@ -8,10 +8,12 @@ namespace api.Services.PrometheusService
     public class PrometheusService : IPrometheusService
     {
         private static readonly Counter RequestCounter = Metrics.CreateCounter("prometheus_requests_total", "Total number of requests for Promdata.");
+        private static readonly Gauge ActiveRequests = Metrics.CreateGauge("prometheus_active_requests", "Number of active requests for Promdata.");
 
         public IEnumerable<Prom> GetProms()
         {
             RequestCounter.Inc(); // Increment the counter each time the method is called
+            ActiveRequests.Inc(); // Increment the guage to indicate an active request
 
             return new List<Prom>
             {
